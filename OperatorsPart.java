810ba3891088fc816b1315465e0a62ae05e8653c
Johnson-Nwi-ue Great <yerekajohnson@gmail.com>
2025-10-28 20:38:32 +0100

    Updated OperatorsPart.java

---
+++ OperatorsPart.java
@@ -0,0 +1,30 @@
+  //Arithmetic operators
+   int num1 = 10;
+   int num2 = 4;
+   String name = "Martins";
+   
+   
+   int sum = num1 + num2;
+   int product = num1 * num2;
+   int minus = num1 - num2;
+   double divide = (double)num1/num2;
+   
+   
+   System.out.printf("Sum is %d%n" ,sum);
+   System.out.printf("Product id %d%n", product);
+   System.out.printf("difference between num1 and num2 is %d%n",minus);
+   System.out.println(name + sum);
+   System.out.printf("Result is %d",divide);  
+   System.out.printf("Modulos is %d", modulos);
+   
+   //Relational operators
+   int first num =  12;
+   int second num = 15;
+   boolean isGreater = firstNum > secondNum;
+   System.out.printf(" is firstNum > secondNum? %b%n", isGreater);
+   System.out.printf("is firstNum < secondNum? %b%n", isLesser);
+   System.out.printf("is firstname >= secondNum? %b%n",isGreaterThanOrEqualTo);
+   System.out.printf("is firstname < secondNum? %b%n", isLessThanOrEqualTo);
+   System.out.printf("is firstname != secondNum? %b%n", isNotEqualTo);
+   System.out.printf("is firstname < secondNum? %b%n", isEqualTo);
+   
