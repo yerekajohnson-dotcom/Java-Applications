77e4287c8905cae7f6d33293dbdf0bf900804654
Johnson-Nwi-ue Great <yerekajohnson@gmail.com>
2025-10-29 19:59:59 +0100

    Initial commit

---
+++ SelectionStatement.java
@@ -0,0 +1,9 @@
+public class seletionstatement{
+    public static void main(string[] args){
+	
+	
+	    int age = 18; 
+		
+		//isngle selection statement 
+		if(age >= 18)
+		    system.out.printIn("Wow, you are an adult"
