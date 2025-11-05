12e7671ec0a3078841c5923f92c2573d104a6003
davidbethuel29-stack <davidbethuel29@gmail.com>
2025-11-05 20:13:07 +0100

    Initial commit

---
+++ BreakStatement.java
@@ -0,0 +1,7 @@
+public class BreakStatement{
+   public static void main(String[] args) {
+          for(int i = 1; i <= 20; i++) {
+		     System.out.println("Testing the break statement");
+			 if(i == 15) {
+				 System.out.println("The loop terminate here");
+				 break;
